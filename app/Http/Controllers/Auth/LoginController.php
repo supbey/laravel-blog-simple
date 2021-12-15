d973b7c579ad7c50b77a30e2a0b8ce0c3e5ec53d
supbey <supbey@qq.com>
2021-12-15 09:29:06 +0800

    add

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    //protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('admin.auth.login');
+    }
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/admin';
+
+    // 退出后重定向到登录页
+    public function loggedOut(Request $request)
+    {
+        return redirect('/login');
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,20 @@ Route::get('/blog', [BlogController::class, 'index'])->name('blog.home');
 Route::get('/blog/{slug}', [BlogController::class, 'showPost'])->name('blog.detail');
 
 
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+// 后台路由
+Route::get('/admin', function () {
+    return redirect('/admin/post');
+});
+Route::middleware('auth')->namespace('App\Http\Controllers\Admin')->group(function () {
+    Route::resource('admin/post', 'PostController');
+    Route::resource('admin/tag', 'TagController');
+    Route::get('admin/upload', 'UploadController@index');
+});
+// 登录退出
+Route::get('/login', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login');
+Route::post('/login', 'App\Http\Controllers\Auth\LoginController@login');
+Route::get('/logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
